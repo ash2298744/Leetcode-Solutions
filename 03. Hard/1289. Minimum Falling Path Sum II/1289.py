c49823fe8ec449b3dad481fdc7a5aca222be2fca
Ashish Gupta <ash75356@gmail.com>
2024-04-26 22:53:34 +0530

    update

--- 03. Hard/1289. Minimum Falling Path Sum II/1289.py
+++ 03. Hard/1289. Minimum Falling Path Sum II/1289.py
@@ -1,18 +1,14 @@
 class Solution:
     def minFallingPathSum(self, grid: List[List[int]]) -> int:
-        n = len(grid)
-        
-        curr = [inf for _ in range(n + 1)]
-        nxt = [inf for _ in range(n + 1)]
+        n=len(grid)
+        dp=[[inf for i in range(n+1)] for i in range(n+1)]
 
         for i in range(n):
-            curr[i] = grid[n - 1][i]
+            dp[n-1][i]=grid[n-1][i]
 
-        for i in range(n - 2, -1, -1):
-            for j in range(n - 1, -1, -1):
+        for i in range(n-2,-1,-1):
+            for j in range(n-1,-1,-1):
                 for k in range(n):
-                    if j != k:
-                        curr[j] = min(curr[j], grid[i][j] + nxt[k])
-            nxt = [x for x in curr]
-        print(nxt)
-        return curr[n - 1]
+                    if j!=k:
+                        dp[i][j]=min(dp[i][j],grid[i][j]+dp[i+1][k])
+        return min(dp[0])
