1e945c2f2cd24a59590a4f3a4cc7bd525c11bed5
Ashish Gupta <ash75356@gmail.com>
2024-05-18 21:18:06 +0530

    update

--- 02. Medium/129. Sum Root to Leaf Numbers/129.py
+++ 02. Medium/129. Sum Root to Leaf Numbers/129.py
@@ -7,7 +7,9 @@
 class Solution:
     def sumNumbers(self, root: Optional[TreeNode]) -> int:
         def dfs(node, val):
-            if not node: return 0
-            if not node.left and not node.right: return val * 10 + node.val
+            if not node: 
+                return 0
+            if not node.left and not node.right: 
+                return val * 10 + node.val
             return dfs(node.left, val * 10 + node.val) + dfs(node.right, val * 10 + node.val)
         return dfs(root, 0)
