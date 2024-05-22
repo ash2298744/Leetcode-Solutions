8aff5ec3b3cb2c8fa47399971769a7ded1b3c4d3
Ashish Gupta <ash75356@gmail.com>
2024-05-22 22:33:06 +0530

    update

--- 02. Medium/525. Contiguous Array/525.py
+++ 02. Medium/525. Contiguous Array/525.py
@@ -2,7 +2,8 @@ class Solution:
     def findMaxLength(self, nums: List[int]) -> int:
         dp = defaultdict(int)
         dp[0] = -1
-        ans = val = 0
+        ans = 0
+        val = 0
         for i in range(len(nums)):
             val += 1 if nums[i] else -1
             if val in dp: 
