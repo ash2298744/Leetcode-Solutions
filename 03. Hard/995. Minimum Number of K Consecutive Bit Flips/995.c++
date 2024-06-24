8b22e90256b1ba1d138a562f70a4c2ace02aa613
Ashish Gupta <ash75356@gmail.com>
2024-06-24 22:41:57 +0530

    update

---
+++ 03. Hard/995. Minimum Number of K Consecutive Bit Flips/995.c++
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int minKBitFlips(vector<int>& nums, int k) {
+        int n = nums.size(), res = 0;
+
+        vector<int> cnt(n + 1, 0);
+        for(int i = 0; i < n - k + 1; i++) {
+            if(cnt[i] % 2) {
+                if(nums[i]) {
+                    cnt[i]++;
+                    cnt[i + k]--;
+                    res++;
+                }
+            } else {
+                if(!nums[i]) {
+                    cnt[i]++;
+                    cnt[i + k]--;
+                    res++;
+                }
+            }
+            cnt[i + 1] += cnt[i];
+        }
+
+        for(int i = n - k + 1; i < n; i++) {
+            cnt[i + 1] += cnt[i];
+            if(cnt[i] % 2 == nums[i]) return -1;
+        }
+        return res;
+    }
+};
---
+++ 03. Hard/995. Minimum Number of K Consecutive Bit Flips/995.py
@@ -0,0 +1,25 @@
+class Solution:
+    def minKBitFlips(self, nums: List[int], k: int) -> int:
+        n = len(nums)
+        cnt = [0 for _ in range(n + 1)]
+        res = 0
+        for i in range(n - k + 1):
+            if cnt[i] % 2:
+                if nums[i] == 1:
+                    cnt[i] += 1
+                    cnt[i + k] -= 1
+                    nums[i] = 1
+                    res += 1
+            else:
+                if nums[i] == 0:
+                    cnt[i] += 1
+                    cnt[i + k] -= 1
+                    nums[i] = 1
+                    res += 1
+            cnt[i + 1] += cnt[i]
+        
+        for i in range(n - k + 1, n):
+            cnt[i + 1] += cnt[i]
+            if cnt[i] % 2 == nums[i]:
+                return -1
+        return res
